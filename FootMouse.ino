fce90596e15768c6db23dc24d1e2525bdd6e40a1
George Kuegler <georgekuegler@gmail.com>
2021-08-15 21:49:43 -0400

    attempt but fail

--- FootMouse.ino
+++ FootMouse.ino
@@ -8,6 +8,13 @@ you must set usb type under Tools -> Board -> USB Type to include
 a Serial library.
 e.g. "Serial + Keyboard + Mouse + Joystick"
 
+Serial Update:
+sequence of ops:
+send single byte message to server
+server checks current coordinates of mouse
+if mouse is in left-hand area of screen
+send single bite message back
+    to release the middle click
 
 */
 
@@ -32,13 +39,17 @@ e.g. "Serial + Keyboard + Mouse + Joystick"
 // Writing the values is redundant
 // outside programs need to know these values
 // so i have them listed explicitly
-enum message_value {
+enum return_message_value {
     LEFT_CLICK = 0,
     LEFT_PRESS = 1,
     LEFT_RELEASE = 2,
     MIDDLE_CLICK = 3,
     MIDDLE_PRESS = 4,
     MIDDLE_RELEASE = 5
+};
+
+enum send_message_value {
+    MIDDLE_DISABLE = 11,
 };
 
 byte iButtonResetCount = 0; // main loop will reset button after count
@@ -56,6 +67,7 @@ byte rCountLeft = 0;
 // Variables for middle button
 byte iButtonResetCountMiddle = 0; // main loop will reset button after count
 bool bStateMiddle = DGTL_READ_PRESSED_STATE;
+volatile bool bPressedFlag = 0;
 
 volatile bool PressedMiddle = 0; // 0 is not PressedLeft, IE pulled high
 volatile bool ActivatedMiddle = 0; // 0 is not PressedLeft, IE pulled high
@@ -63,6 +75,7 @@ unsigned long ChangeTimeMiddle = 0;
 unsigned long ReleasedTimeMiddle = 0;
 unsigned long InterruptTimeMiddle = 0;
 unsigned long ResetTimeMiddle = 0;
+unsigned long RoundTrip = 0;
 byte pCountMiddle = 0;
 byte rCountMiddle = 0;
 
@@ -78,30 +91,41 @@ void log(T msg)
     }
 }
 
+bool checkIfMouseInArea()
+{   
+    Serial.send_now();
+    Serial.write(MIDDLE_DISABLE);
+    Serial.available();
+    IncomingByte = Serial.read();
+
+    if (MIDDLE_DISABLE == IncomingByte) return true;
+    else return false;
+}
+
 void buttonChangedLeft()
 {
     //Serial.println("Change"); // for debugging
     InterruptTimeLeft = millis();
     if (PressedLeft == 0) {
-        log("change int left press");
+        // log("change int left press");
         if (InterruptTimeLeft - ChangeTimeLeft > BOUNCE_TIME)
         {
             Mouse.press(MOUSE_LEFT);
             delay(1);
             PressedLeft = 1;
             ChangeTimeLeft = InterruptTimeLeft;
-            log(++pCountLeft); // for debugging
+            // log(++pCountLeft); // for debugging
         }
     }
     if (PressedLeft == 1) {
-        log("change int left release");
+        // log("change int left release");
         if (InterruptTimeLeft - ChangeTimeLeft > BOUNCE_TIME)
         {
             Mouse.release(MOUSE_LEFT);
             delay(1);
             PressedLeft = 0;
             ChangeTimeLeft = InterruptTimeLeft;
-            log(++rCountLeft); // for debugging
+            // log(++rCountLeft); // for debugging
         }
     }
 }
@@ -111,18 +135,21 @@ void buttonChangedMiddle()
     //Serial.println("Change"); // for debugging
     InterruptTimeMiddle = millis();
     if (PressedMiddle == 0) {
-        log("change int middle press");
+        // log("change int middle press");
         if (InterruptTimeMiddle - ChangeTimeMiddle > BOUNCE_TIME)
         {
-            Mouse.press(MOUSE_MIDDLE);
-            delay(1);
+            // Don't press the middle mouse button
+            // if in the designated area
+            // Mouse.press(MOUSE_MIDDLE);
+            // delay(1);
+            bPressedFlag = 1;
             PressedMiddle = 1;
             ChangeTimeMiddle = InterruptTimeMiddle;
-            log(++pCountMiddle); // for debugging
+            // log(++pCountMiddle); // for debugging
         }
     }
     if (PressedMiddle == 1) {
-        log("change int middle release");
+        // log("change int middle release");
         if (InterruptTimeMiddle - ChangeTimeMiddle > BOUNCE_TIME)
         {
             Mouse.release(MOUSE_MIDDLE);
@@ -130,7 +157,7 @@ void buttonChangedMiddle()
             PressedMiddle = 0;
             ChangeTimeMiddle = InterruptTimeMiddle;
             ReleasedTimeMiddle = InterruptTimeMiddle;
-            log(++rCountMiddle); // for debugging
+            // log(++rCountMiddle); // for debugging
         }
     }
 }
@@ -153,6 +180,17 @@ void setup()
 
 void loop()
 {
+    // Pick up the ISR flag press the middle button
+    if (bPressedFlag)
+    {
+        if (checkIfMouseInArea())
+        {
+            Mouse.press(MOUSE_MIDDLE);
+            bPressedFlag = 0;
+        }
+
+    }
+
     if (iButtonResetCount++ == 100)
     {
         bStateLeft = digitalRead(LBUTTON_PIN);
@@ -197,17 +235,17 @@ void loop()
     // Check the serial port for incoming bites every loop
     // if (Serial.available())
     // {
-    IncomingByte = Serial.read();
-    if (-1 != IncomingByte)
-    {
-        switch (IncomingByte)
-        {
-            case MIDDLE_RELEASE:
-                Mouse.release(MOUSE_MIDDLE);
-                break;
-            default:
-                break;
-        }
-    }
+    // IncomingByte = Serial.read();
+    // if (-1 != IncomingByte)
+    // {
+    //     switch (IncomingByte)
+    //     {
+    //         case MIDDLE_RELEASE:
+    //             Mouse.release(MOUSE_MIDDLE);
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
     // }
 }
