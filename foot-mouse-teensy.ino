e442ed39f935dac27c7ad934ce5243ca3b477efe
George Kuegler <georgekuegler@gmail.com>
2021-09-18 20:38:57 -0400

    add serial communication to change middle pedal function

--- foot-mouse-teensy.ino
+++ foot-mouse-teensy.ino
@@ -37,8 +37,8 @@ send single bite message back
 #define DEBUG 1
 
 static_assert(MOUSE_LEFT == 1, "Voice commands will fail");
-static_assert(MOUSE_RIGHT == 4, "Voice commands will fail");
-static_assert(MOUSE_MIDDLE == 2, "Voice commands will fail");
+static_assert(MOUSE_MIDDLE == 4, "Voice commands will fail");
+static_assert(MOUSE_RIGHT == 2, "Voice commands will fail");
 
 // Writing the values is redundant
 // outside programs need to know these values
@@ -65,8 +65,6 @@ void log(T msg)
     }
 }
 
-int IncomingByte = 0;
-
 class CButton{
 public:
     int buttonPin;
@@ -92,9 +90,132 @@ CButton buttonArray[] = {
     CButton(RBUTTON_PIN, MOUSE_RIGHT, false)
 };
 
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 6;
-int buttonState = 0;
+const byte bufferSize = 3;
+byte inputBuffer[bufferSize];
+bool newMessageAvailable = false;
+
+void ReceiveSerialInput()
+{
+    static bool receiveInProgress = false;
+    static byte index = 0;
+    // static byte startMarker = '<';
+    // static byte endMarker = '>'
+    static byte startMarker = 16;
+    static byte endMarker = 19;
+    static byte rb;
+
+    index = 0;
+    receiveInProgress = false;
+
+    if (newMessageAvailable)
+    {h
+        Log("message rejected, existing message not read yet");
+        return;
+    }
+
+
+    if (Serial.available()) 
+    {
+        Log("starting to read message");
+        while (Serial.available() > 0)
+        {
+            rb = Serial.read();
+            Log(rb);
+
+            if (receiveInProgress)
+            {
+                if (rb != endMarker)
+                {
+                    Log("adding character to buffer");
+                    inputBuffer[index] = rb;
+                    index++;
+
+                    if (index >= 5) 
+                    {
+                        Log("buffer overflow on input");
+                        return;
+                    }
+                }
+                else break;
+            }
+            else if (rb == startMarker) receiveInProgress = true;
+        }
+        Log("end of the loop reached");
+
+        receiveInProgress = false;
+
+        if (index == 0) Log("no start sequence found");
+        else if (index < 3) Log("message not long enough");
+        else
+        {
+            newMessageAvailable = true;
+            Log("Setting message available: true");
+        }
+    }
+
+}
+
+void ParseMessage()
+{
+    Log("Starting To Parse Message");
+    byte pedalNumber = inputBuffer[0];
+    byte mouseMode = inputBuffer[1];
+    byte isInverted = inputBuffer[2];
+
+    Log(pedalNumber);
+    Log(mouseMode);
+    Log(isInverted);
+
+    if (pedalNumber >= 0 && pedalNumber <= 2)
+    {
+        if (mouseMode == MOUSE_LEFT || mouseMode == MOUSE_MIDDLE || mouseMode == MOUSE_RIGHT)
+        {
+            buttonArray[pedalNumber].mode = mouseMode;
+        }
+        else Log("Mouse mode is not valid.");
+
+        if (isInverted == 0 || isInverted == 1)
+        {
+            buttonArray[pedalNumber].isInverted = isInverted;
+        }
+        else Log("Inverted mode is not valid.");
+    }
+    else Log("Petal number is not valid.");
+
+    newMessageAvailable = false;
+    Log("Ending Parse message");
+}
+
+// void ParseMessage()
+// {
+//     Log("Starting To Parse Message");
+//     byte pedalNumber = inputBuffer[0];
+//     byte mouseMode = inputBuffer[1];
+//     byte isInverted = inputBuffer[2];
+
+//     Log(pedalNumber);
+//     Log(mouseMode);
+//     Log(isInverted);
+
+//     if (pedalNumber == '0')
+//     {
+//         if (mouseMode == 'L')
+//         {
+//             buttonArray[1].mode = MOUSE_LEFT;
+//         }
+//         else Log("Mouse mode is not valid.");
+
+//         if (isInverted == '0')
+//         {
+//             buttonArray[1].isInverted = 0;
+//         }
+//         else Log("Inverted mode is not valid.");
+//     }
+//     else Log("Pedal number is not valid.");
+
+//     newMessageAvailable = false;
+//     
+// }
 
 void setup()
 {
@@ -141,4 +262,14 @@ void loop()
             }
         }
     }
+
+    // Enable programs on my PC to alter the behavior
+    // of my foot mouse.
+    // Check the serial port for incoming bites every loop
+    ReceiveSerialInput();
+    if (newMessageAvailable)
+    {
+        ParseMessage();
+    }
+
 }
